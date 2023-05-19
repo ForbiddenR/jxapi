b21639a3528b73d4d6265b5842159f4f225d991e
ForbiddenR <hostyouposition@outlook.com>
2023-05-19 15:59:48 +0800

    add two fields to the remote stop transaciton command

--- jxservices/v1/equip/remote_stop_transaction.go
+++ jxservices/v1/equip/remote_stop_transaction.go
@@ -1,6 +1,11 @@
 package equip
 
-import services "github.com/ForbiddenR/jxapi/jxservices"
+import (
+	"encoding/json"
+	"errors"
+
+	services "github.com/ForbiddenR/jxapi/jxservices"
+)
 
 type EquipRemoteStopTransactionRequest struct {
 	services.Base
@@ -8,5 +13,31 @@ type EquipRemoteStopTransactionRequest struct {
 }
 
 type EquipRemoteStopTransactionRequestDetail struct {
-	TransactionId string `json:"transactionId" validate:"required"`
+	TransactionId string  `json:"transactionId" validate:"required"`
+	EvseId        *string `json:"evseSerial"`
+	ConnectorId   *string `json:"connectorSerial"`
+}
+
+func (r *EquipRemoteStopTransactionRequest) UnmarshalJSON(data []byte) error {
+	type Alias EquipRemoteStopTransactionRequest
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(r),
+	}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	if aux.Data == nil {
+		return errors.New("data is nil")
+	}
+	if !aux.Protocol.Equal(services.OCPP16()) {
+		if aux.Data.EvseId == nil {
+			return errors.New(aux.Protocol.Name + ":evse serial is nil")
+		}
+		if aux.Data.ConnectorId == nil {
+			return errors.New(aux.Protocol.Name + ":connector serial is nil")
+		}
+	}
+	return nil
 }
