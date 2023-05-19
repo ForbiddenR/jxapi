e9627afbc65156eb816a0c93df7a6c206d288075
ForbiddenR <hostyouposition@outlook.com>
2023-05-19 11:25:44 +0800

    add datatransfer feature from platform

---
+++ jxservices/v1/equip/data_transfer_platform.go
@@ -0,0 +1,32 @@
+package equip
+
+import (
+	"encoding/json"
+
+	services "github.com/ForbiddenR/jxapi/jxservices"
+)
+
+type EquipDataTransferRequest struct {
+	services.Base
+	Data *EquipDataTransferRequestDetail `json:"data"`
+}
+
+type EquipDataTransferRequestDetail struct {
+	VendorId  string `json:"vendorId"`
+	Data      string `json:"data"`
+	MessageId string `json:"messageId"`
+}
+
+func (r *equipDataTransferRequest) UnmarshalJSON(b []byte) error {
+	type Alias equipDataTransferRequest
+	aux := &struct {
+		*Alias
+	}{
+		Alias: (*Alias)(r),
+	}
+
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ jxservices/v1/equip/data_transfer_platform_callback.go
@@ -0,0 +1,44 @@
+package equip
+
+import (
+	"context"
+
+	api "github.com/ForbiddenR/jxapi"
+	services "github.com/ForbiddenR/jxapi/jxservices"
+)
+
+type equipDataTransferRequest struct {
+	services.Base
+	Callback *equipDataTransferRequestDetail `json:"data"`
+}
+
+type equipDataTransferRequestDetail struct {
+}
+
+func (r equipDataTransferRequest) GetName() string {
+	return services.DataTransfer.String()
+}
+
+type equipDataTransferResponse struct {
+	api.Response
+	Data *equipDataTransferResponseDetail `json:"data"`
+}
+
+type equipDataTransferResponseDetail struct {
+}
+
+func (resp *equipDataTransferResponse) GetStatus() int {
+	return resp.Status
+}
+
+func (resp *equipDataTransferResponse) GetMsg() string {
+	return resp.Msg
+}
+
+func DataTransferRequest(ctx context.Context, req services.CallbackRequest) error {
+	header := services.GetCallbackHeaderValue(services.DataTransfer)
+
+	url := services.GetCallbackURL(req)
+
+	return services.RequestWithoutResponse(ctx, req, url, header, &equipDataTransferResponse{})
+}
