fdf507da2d20655babc48201e2726bd8bfd34024
ForbiddenR <hostyouposition@outlook.com>
2023-05-29 14:47:32 +0800

    update

--- jxservices/v1/equip/update_transaction.go
+++ jxservices/v1/equip/update_transaction.go
@@ -19,10 +19,37 @@ func (equipUpdateTransactionRequest) GetName() string {
 }
 
 type equipUpdateTransactionReqeustDetail struct {
+	TransactionId string      `json:"transactionId"`
+	EvseId        *string     `json:"evseSerial"`
+	ConnectorId   string      `json:"connectorSerial"`
+	Offline       bool        `json:"offline"`
+	Timestamp     int64       `json:"timestamp"`
+	MeterValue    *MeterValue `json:"MeterValue"`
+	Tariff        Tariff      `json:"tariff"`
+	ChargingState uint8       `json:"chargingState"`
+	VIN           string      `json:"vin"`
 }
 
-func NewUpdateTransactionRequest(sn, pod, msgID string, p *services.Protocol) *equipUpdateTransactionRequest {
-	updateTransaction := &equipUpdateTransactionRequest{}
+func NewUpdateTransactionRequest(sn, pod, msgID string, p *services.Protocol, transactionId, connectorId, vin string, offline bool, timestamp int64, chargeStation uint8) *equipUpdateTransactionRequest {
+	updateTransaction := &equipUpdateTransactionRequest{
+		Base: services.Base{
+			EquipmentSn: sn,
+			Protocol:    p,
+			Category:    services.UpdateTransaction.FirstUpper(),
+			AccessPod:   pod,
+			MsgID:       msgID,
+		},
+		Data: &equipUpdateTransactionReqeustDetail{
+			TransactionId: transactionId,
+			ConnectorId:   connectorId,
+			Offline:       offline,
+			Timestamp:     timestamp,
+			MeterValue:    &MeterValue{},
+			Tariff:        Tariff{},
+			ChargingState: chargeStation,
+			VIN:           vin,
+		},
+	}
 
 	return updateTransaction
 }
