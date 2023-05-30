0d390284f0ca332b4fa9ef95f957604476813375
ForbiddenR <hostyouposition@outlook.com>
2023-05-30 14:44:28 +0800

    update the structure

--- jxservices/v1/equip/update_transaction.go
+++ jxservices/v1/equip/update_transaction.go
@@ -19,18 +19,18 @@ func (equipUpdateTransactionRequest) GetName() string {
 }
 
 type equipUpdateTransactionReqeustDetail struct {
-	TransactionId string      `json:"transactionId"`
-	EvseId        *string     `json:"evseSerial"`
-	ConnectorId   string      `json:"connectorSerial"`
-	Offline       bool        `json:"offline"`
-	Timestamp     int64       `json:"timestamp"`
-	MeterValue    *MeterValue `json:"MeterValue"`
-	Tariff        Tariff      `json:"tariff"`
-	ChargingState uint8       `json:"chargingState"`
+	TransactionId string       `json:"transactionId"`
+	EvseId        *string      `json:"evseSerial"`
+	ConnectorId   string       `json:"connectorSerial"`
+	Offline       bool         `json:"offline"`
+	Timestamp     int64        `json:"timestamp"`
+	MeterValue    []MeterValue `json:"MeterValue"`
+	Tariff        Tariff       `json:"tariff"`
+	ChargingState uint8        `json:"chargingState"`
 	VIN           *string      `json:"vin"`
 }
 
-func NewUpdateTransactionRequest(sn, pod, msgID string, p *services.Protocol, transactionId, connectorId string, offline bool, timestamp int64, chargeStation uint8) *equipUpdateTransactionRequest {
+func NewUpdateTransactionRequest(sn, pod, msgID string, p *services.Protocol, transactionId, connectorId string, offline bool, timestamp int64, chargeState uint8) *equipUpdateTransactionRequest {
 	updateTransaction := &equipUpdateTransactionRequest{
 		Base: services.Base{
 			EquipmentSn: sn,
@@ -44,9 +44,9 @@ func NewUpdateTransactionRequest(sn, pod, msgID string, p *services.Protocol, tr
 			ConnectorId:   connectorId,
 			Offline:       offline,
 			Timestamp:     timestamp,
-			MeterValue:    &MeterValue{},
+			MeterValue:    make([]MeterValue, 0),
 			Tariff:        Tariff{},
-			ChargingState: chargeStation,
+			ChargingState: chargeState,
 		},
 	}
 
