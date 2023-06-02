f6049415ae28d7ab2e7ab23b1777688d062b5ac3
ForbiddenR <hostyouposition@outlook.com>
2023-06-02 13:51:59 +0800

    add fields to SetChargingProfile

--- jxservices/v1/equip/set_charging_profile.go
+++ jxservices/v1/equip/set_charging_profile.go
@@ -10,6 +10,32 @@ type EquipSetChargingProfileRequest struct {
 	services.Base
 }
 
+type EquipSetChargingProfileRequestDetail struct {
+	EvseId          string           `json:"evseId"`
+	ConnectorId     string           `json:"connectorId"`
+	ChargingProfile *ChargingProfile `json:"chargingProfile,omitempty"`
+}
+
+type ChargingProfile struct {
+	Id               uint64              `json:"id,omitempty"`
+	ValidFrom        string              `json:"validFrom,omitempty"`
+	ValidTo          string              `json:"validTo,omitempty"`
+	TransactionID    string              `json:"transactionID,omitempty"`
+	StartTime        string              `json:"startTime,omitempty"`
+	ChargingSchedule []*ChargingSchedule `json:"chargingSchedule,omitempty"`
+}
+
+type ChargingSchedule struct {
+	ChargingRateUnit       int32                         `json:"chargingRateUnit,omitempty"`
+	ChargingSchedulePeriod []*ChargingSchedulePeriodType `protobuf:"bytes,2,rep,name=chargingSchedulePeriod,proto3" json:"chargingSchedulePeriod,omitempty"`
+}
+
+type ChargingSchedulePeriodType struct {
+	From  int64 `json:"from,omitempty"`
+	Limit int32 `json:"limit,omitempty"`
+	To    int64 `json:"to,omitempty"`
+}
+
 func (r *EquipSetChargingProfileRequest) UnmarshalJSON(data []byte) error {
 	type Alias EquipSetChargingProfileRequest
 	aux := &struct {
