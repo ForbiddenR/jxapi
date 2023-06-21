3c939b06854bc8372b642fe01f379025e004591a
ForbiddenR <hostyouposition@outlook.com>
2023-06-21 01:04:36 -0400

    fill the fields into ChargeEncryInfoNotification

--- jxservices/v1/equip/charge_encry_info_notification.go
+++ jxservices/v1/equip/charge_encry_info_notification.go
@@ -13,13 +13,21 @@ type equipChargeEncryInfoNotificationRequest struct {
 }
 
 type equipChargeEncryInfoNotificationRequestDetail struct {
+	EvseId           *string `json:"evseSerial"`
+	ConnectorId      string  `json:"connectorSerial"`
+	TransactionId    string  `json:"transactionId"`
+	EncryptedData    string  `json:"encryptedData"`
+	MeterNum         string  `json:"meterNum"`
+	ElecmeterVersion int64   `json:"elecmeterVersion"`
+	EncryType        uint64  `json:"encryType"`
 }
 
 func (equipChargeEncryInfoNotificationRequest) GetName() string {
 	return services.ChargeEncryInfoNotification.String()
 }
 
-func NewEquipChargeEncryInfoNotificationRequest(sn, pod, msgID string, p *services.Protocol) *equipChargeEncryInfoNotificationRequest {
+func NewEquipChargeEncryInfoNotificationRequest(sn string, p *services.Protocol, pod, msgID string,
+	connectorId, transactionId, encryptedData, meterNum string, elecmeterVersion int64, encryType uint64) *equipChargeEncryInfoNotificationRequest {
 	req := &equipChargeEncryInfoNotificationRequest{
 		Base: services.Base{
 			EquipmentSn: sn,
@@ -28,7 +36,14 @@ func NewEquipChargeEncryInfoNotificationRequest(sn, pod, msgID string, p *servic
 			AccessPod:   pod,
 			MsgID:       msgID,
 		},
-		Data: &equipChargeEncryInfoNotificationRequestDetail{},
+		Data: &equipChargeEncryInfoNotificationRequestDetail{
+			ConnectorId:      connectorId,
+			TransactionId:    transactionId,
+			EncryptedData:    encryptedData,
+			MeterNum:         meterNum,
+			ElecmeterVersion: elecmeterVersion,
+			EncryType:        encryType,
+		},
 	}
 	return req
 }
