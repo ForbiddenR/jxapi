af57765768015eda6b2b5e4b6af2c19fb8b3926d
ForbiddenR <hostyouposition@outlook.com>
2023-06-28 04:11:08 -0400

    adjust some arguments.

--- jxservices/v1/equip/unsupport.go
+++ jxservices/v1/equip/unsupport.go
@@ -8,11 +8,14 @@ import (
 	services "github.com/ForbiddenR/jxapi/jxservices"
 )
 
-func UnspportedCallbackRequest(ctx context.Context, name, sn, pod, msgID string, p *services.Protocol, err *apierrors.CallbackError) error {
+func UnspportedCallbackRequest(ctx context.Context, name, sn, pod, msgID string, p *services.Protocol) error {
 	fn, ok := services.FetchFC(name)
 	if !ok {
 		return fmt.Errorf("this feature hasn't implemented. name: %s, id: %s", name, msgID)
 	}
+
+	err := apierrors.NewCallbackErrorNotSupported(sn, name)
+
 	req := fn(sn, pod, msgID, p, err)
 
 	header := services.GetSimpleHeaderValue(services.Request2ServicesNameType(req.GetName()))
