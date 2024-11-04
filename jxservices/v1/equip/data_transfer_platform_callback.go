3888d6eab57a0f5f05b8c15781a0018743390539
zhangsan3356 <oamsic@163.com>
2024-11-04 10:13:08 +0800

    remove an unused struct

--- jxservices/v1/equip/data_transfer_platform_callback.go
+++ jxservices/v1/equip/data_transfer_platform_callback.go
@@ -3,7 +3,6 @@ package equip
 import (
 	"context"
 
-	api "github.com/ForbiddenR/jxapi/v2"
 	services "github.com/ForbiddenR/jxapi/v2/jxservices"
 )
 
@@ -32,21 +31,21 @@ func NewEquipDataTransferRequest() *equipDataTransferRequest {
 	return req
 }
 
-type equipDataTransferResponse struct {
-	api.Response
-	Data *equipDataTransferResponseDetail `json:"data"`
-}
+// type equipDataTransferResponse struct {
+// 	api.Response
+// 	Data *equipDataTransferResponseDetail `json:"data"`
+// }
 
-type equipDataTransferResponseDetail struct {
-}
+// type equipDataTransferResponseDetail struct {
+// }
 
-func (resp *equipDataTransferResponse) GetStatus() int {
-	return resp.Status
-}
+// func (resp *equipDataTransferResponse) GetStatus() int {
+// 	return resp.Status
+// }
 
-func (resp *equipDataTransferResponse) GetMsg() string {
-	return resp.Msg
-}
+// func (resp *equipDataTransferResponse) GetMsg() string {
+// 	return resp.Msg
+// }
 
 func DataTransferRequest(ctx context.Context, req services.Request) error {
 	return services.Transport(ctx, req)
