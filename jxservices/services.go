33caebe920cc9f130a9a394461dae205c9f6d6d3
zhangsan3356 <oamsic@163.com>
2024-11-19 16:14:59 +0800

    fix a spelling error

--- jxservices/services.go
+++ jxservices/services.go
@@ -363,7 +363,7 @@ var iec002 = &Protocol{Name: "IEC104", Version: "0.2"}
 var iec003 = &Protocol{Name: "IEC104", Version: "0.3"}
 var iec004 = &Protocol{Name: "IEC104", Version: "0.4"}
 var iec005 = &Protocol{Name: "IEC104", Version: "0.5"}
-var yunKuaiChong = &Protocol{Name: "YunKuangChong", Version: "1"}
+var yunKuaiChong = &Protocol{Name: "YunKuaiChong", Version: "1"}
 var fleetin = &Protocol{Name: "Fleetin", Version: "2.0.5"}
 
 func OCPP16() *Protocol {
